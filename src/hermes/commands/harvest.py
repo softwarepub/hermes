69e465635ed7390d92779add61c37ab6592b620b
Stephan Druskat <stephan.druskat@dlr.de>
2022-06-10 09:55:32 +0200

    Mock CFF harvesting workflow with context

--- src/hermes/commands/harvest.py
+++ src/hermes/commands/harvest.py
@@ -1,2 +1,14 @@
-def harvest_cff():
+def harvest_cff(ctx: HermesContext):
+    # Get file
+    source = cff_source
+
+    # Validate
+
+    # Load
+    cff = load(source)
+
+    # Convert
+    author = cff.get('authors')
+
+    ctx.update('author', author, src=source)
     print('Hello CFF harvester')
