a70131f596e8a94392390d4c00635760bf59aaf0
Stephan Druskat <stephan.druskat@dlr.de>
2022-06-10 10:07:35 +0200

    Mock catching exceptions

--- src/hermes/commands/harvest.py
+++ src/hermes/commands/harvest.py
@@ -2,7 +2,14 @@ def harvest_cff(ctx: HermesContext):
     # Get file
     source = cff_source
 
-    # Validate
+    # Validate via jsonschema
+    try:
+        validate()
+    except Exception as e:
+        raise HermesValidationError('CFF is invalid') from e
+
+    if not valid:
+        ctx.error(msg='Sorry, but your CFF is not valid, here are the errors.', errors=errors, src=source)
 
     # Load
     cff = load(source)
