4800a3821054648034017c7e5023e388d711478a
Oliver Bertuch <o.bertuch@fz-juelich.de>
2022-07-11 18:51:58 +0200

    docs: replace Sphinx configuration #7
    Rebasing the Sphinx configuration on the developments done for
    https://project.software-metadata.pub.
    
    Merging with current conf.py as necessary.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'hermes'
-copyright = '2022, Stephan Druskat, Michael Meinel'
-author = 'Stephan Druskat, Michael Meinel'
+project = 'HERMES Workflow'
+copyright = '2022, HERMES project'
+author = 'Oliver Bertuch, Stephan Druskat, Guido Juckeland, Jeffrey Kelling, Oliver Knodel, Michael Meinel, Tobias Schlauch'
+
+# The full version, including alpha/beta/rc tags
+release = '2022-07-01'
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,9 +31,23 @@ author = 'Stephan Druskat, Michael Meinel'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.extlinks',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.todo',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.githubpages',
     'sphinx.ext.autodoc',
+    'sphinx-favicon',
+    'sphinxcontrib.contentui',
+    'sphinxcontrib.images',
+    'sphinxcontrib.icon',
+    'sphinxemoji.sphinxemoji',
+    "sphinxext.opengraph",
     'myst_parser'
 ]
+
+language = 'en'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -38,17 +55,61 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['Thumbs.db', '.DS_Store']
 
+# Prefix document path to section labels, to use:
+# `path/to/file:heading` instead of just `heading`
+autosectionlabel_prefix_document = True
+
+# MyST parser options
+myst_enable_extensions = [
+    'tasklist',
+    'deflist',
+]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = 'sphinx_book_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = '_static/img/hermes-visual-blue.svg'
+html_title = 'Workflow Docs'
+
+# Custom sidebar templates, maps document names to template names.
+html_sidebars = {
+    "**": ["sidebar-logo.html", "sbt-sidebar-nav.html"]
+}
+
+# Enable and customize the permanent headerlinks with a nice icon (chain symbol from FontAwesome)
+html_permalinks = True
+html_permalinks_icon = "<i class=\"fas fa-link\"></i>"
+
+html_theme_options = {
+    "home_page_in_toc": True,
+    "extra_navbar": "<div>Funded by the <i>Initiative and Networking Fund</i> of the <a \
+                     href='https://www.helmholtz.de/en/about-us/structure-and-governance/initiating-and-networking/' \
+                    target='_blank'>Helmholtz Association</a> in the framework of the \
+                    <a href='https://helmholtz-metadaten.de' targe='_blank'>Helmholtz Metadata \
+                    Collaboration</a></div>",
+    "repository_url": "https://github.com/hermes-hmc/project",
+    "use_repository_button": True,
+}
+
+html_css_files = [
+    'custom.css',
+]
+
+# -- Options for OpenGraph Tags ----------------------------------------------
+
+ogp_site_url = "https://docs.software-metadata.pub/"
+ogp_image = "https://docs.software-metadata.pub/_static/img/opengraph.png"
+ogp_image_alt = "The HERMES key visual on a blue background with pipelines and the Workflow subproject title"
+ogp_description_length = 200
+ogp_type = "website"
