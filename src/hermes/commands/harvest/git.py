b5a20c3d9b41075032bf8bde422ebd271053aa9e
jkelling <j.kelling@hzdr.de>
2022-09-01 16:08:54 +0200

    Apply suggestions from code review
    PEP-ified
    
    Co-authored-by: Michael Meinel <michael.meinel@dlr.de>

--- src/hermes/commands/harvest/git.py
+++ src/hermes/commands/harvest/git.py
@@ -16,22 +16,24 @@ from hermes.model.errors import HermesValidationError
 # TODO: can and should we get this somehow?
 SHELL_ENCODING = 'utf-8'
 
+
 class ConributorData:
     def __init__(self, d: t.List):
         self.name = d[0]
         self.email = set((d[1],))
-        self.tFirst = d[2]
-        self.tLast = self.tFirst
+        self.t_first= d[2]
+        self.t_last= self.t_first
 
     def update(self, d: t.List):
         assert(self.name == d[0])
 
         self.email.add(d[1])
         t = d[2]
-        if t < self.tFirst:
-            self.tFirst = t
-        elif t > self.tFirst:
-            self.tLast = t
+        if t < self.t_first:
+            self.t_first = t
+        elif t > self.t_first:
+            self.t_last = t
+
 
 def harvest_git(click_ctx: click.Context, ctx: HermesHarvestContext):
     """
@@ -46,20 +48,20 @@ def harvest_git(click_ctx: click.Context, ctx: HermesHarvestContext):
         raise RuntimeError('No parent context!')
     path = parent_ctx.params['path']
 
-    gitExe = shutil.which('git')
-    if not gitExe:
+    git_exe = shutil.which('git')
+    if not git_exe:
         raise RuntimeError('Git not available!')
 
-    p = subprocess.run([gitExe, "rev-parse", "--abbrev-ref", "HEAD"], capture_output=True)
+    p = subprocess.run([git_exe, "rev-parse", "--abbrev-ref", "HEAD"], capture_output=True)
     if p.returncode:
         raise RuntimeError("`git branch` command failed with code {}: '{}'!".format(p.returncode, p.stderr.decode(SHELL_ENCODING)))
-    gitBranch = p.stdout.decode(SHELL_ENCODING).strip()
+    git_branch = p.stdout.decode(SHELL_ENCODING).strip()
     # TODO: should we warn or error if the HEAD is detached?
 
     # Get history of currently checked-out branch
     authors = {}
     committers = {}
-    p = subprocess.run([gitExe, "log", "--pretty=%an_%ae_%at_%cn_%ce_%ct"], capture_output=True)
+    p = subprocess.run([git_exe, "log", "--pretty=%an_%ae_%at_%cn_%ce_%ct"], capture_output=True)
     if p.returncode:
         raise RuntimeError("`git log` command failed with code {}: '{}'!".format(p.returncode, p.stderr.decode(SHELL_ENCODING)))
 
@@ -76,18 +78,19 @@ def harvest_git(click_ctx: click.Context, ctx: HermesHarvestContext):
         _updateContributor(authors, d[0:3])
         _updateContributor(committers, d[3:7])
     
-    _ctxUpdateContributors(ctx, authors, "author", branch=gitBranch)
-    _ctxUpdateContributors(ctx, committers, "committer", branch=gitBranch)
+    _ctx_update_contributors(ctx, authors, "author", branch=git_branch)
+    _ctx_update_contributors(ctx, committers, "committer", branch=git_branch)
 
-def _updateContributor(contributors: t.Dict, d: t.List):
+def _update_contributor(contributors: t.Dict, d: t.List):
     if d[0] in contributors:
         contributors[d[0]].update(d[0:3])
     else:
         contributors[d[0]] = ConributorData(d[0:3])
 
-def _ctxUpdateContributors(ctx: HermesHarvestContext, contributors: t.Dict, kind: str, **kwargs):
+
+def _ctx_update_contributors(ctx: HermesHarvestContext, contributors: t.Dict, kind: str, **kwargs):
     for a in contributors.values():
-        ctx.update(f"{kind}.since", a.tFirst, name=a.name, **kwargs)
-        ctx.update(f"{kind}.until", a.tLast, name=a.name, **kwargs)
+        ctx.update(f"{kind}.since", a.t_first, name=a.name, **kwargs)
+        ctx.update(f"{kind}.until", a.t_last, name=a.name, **kwargs)
         for e in a.email:
             ctx.update(f"{kind}.email", e, name=a.name, email=e, **kwargs)
