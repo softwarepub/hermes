b1166e7fb20d9b6f258f6bcbdf3ea47ccd9a3213
Stephan Druskat <stephan.druskat@dlr.de>
2023-01-12 14:21:31 +0100

    Add git authors and committers as contributors

--- src/hermes/commands/process/git.py
+++ src/hermes/commands/process/git.py
@@ -14,14 +14,14 @@ _AUTHOR_KEYS = ('@id', 'email', 'name')
 
 def flag_authors(ctx: CodeMetaContext, harverst_ctx: HermesHarvestContext):
     """
-    Identify all authors that are not yet in the target context and flag them with role `Contributor`.
+    Add all git authors and committers with role `Contributor`.
 
     :param ctx: The target context containting harmonized data.
     :param harverst_ctx: Data as it was harvested.
     """
     audit_log = logging.getLogger('audit.git')
     audit_log.info('')
-    audit_log.info('### Flag new authors')
+    audit_log.info('### Add git authors and committers as contributors')
 
     author_path = ContextPath('author')
     contributor_path = ContextPath('contributor')
@@ -34,16 +34,10 @@ def flag_authors(ctx: CodeMetaContext, harverst_ctx: HermesHarvestContext):
         return
 
     for i, contributor in enumerate(author_path.get_from(data)):
-        query = {k: contributor[k] for k in _AUTHOR_KEYS if k in contributor}
-        author_key, target, path = author_path['*'].resolve(ctx._data, query=query)
-
-        if author_key._item == '*':
-            audit_log.debug('- %s', contributor['name'])
-            if contributor_path not in ctx.keys():
-                ctx.update(contributor_path, [])
-            ctx.update(contributor_path['*'], contributor, tags=tags)
-        else:
-            ctx.update(author_key, contributor, tags=tags)
+        audit_log.debug('- %s', contributor['name'])
+        if contributor_path not in ctx.keys():
+            ctx.update(contributor_path, [])
+        ctx.update(contributor_path['*'], contributor, tags=tags)
 
     ctx.tags.update(tags)
     harverst_ctx.finish()
