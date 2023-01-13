8b225ec440d0da7ab42e67efbd3b1fd895bc9a9d
Stephan Druskat <stephan.druskat@dlr.de>
2023-01-13 10:19:45 +0100

    Fix context keys to allow for namespaces (colon separator)

--- src/hermes/model/path.py
+++ src/hermes/model/path.py
@@ -19,7 +19,7 @@ class ContextPathGrammar:
     The pyparsing grammar for ContextGrammar paths.
     """
 
-    key = pp.Word('@' + pp.alphas)
+    key = pp.Word('@:' + pp.alphas)
     index = pp.Word(pp.nums).set_parse_action(lambda tok: [int(tok[0])]) | pp.Char('*')
     field = key + (pp.Suppress('[') + index + pp.Suppress(']'))[...]
     path = field + (pp.Suppress('.') + field)[...]
