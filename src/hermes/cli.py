d6a32cbb71396a3c6a47a2aecebb24635ba66ecf
Jeffrey Kelling <j.kelling@hzdr.de>
2023-01-13 11:09:09 +0100

    command clean: make option, remove from default pipeline

--- src/hermes/cli.py
+++ src/hermes/cli.py
@@ -141,6 +141,7 @@ class WorkflowCommand(click.Group):
 @click.group(cls=WorkflowCommand, invoke_without_command=True)
 @click.option("--deposit", is_flag=True, default=False)
 @click.option("--postprocess", is_flag=True, default=False)
+@click.option("--clean", is_flag=True, default=False)
 @click.option('--path', default=pathlib.Path('./'), help='Working path', type=pathlib.Path)
 @click.pass_context
 def main(ctx: click.Context, *args, **kwargs) -> None:
@@ -153,8 +154,8 @@ def main(ctx: click.Context, *args, **kwargs) -> None:
     pass
 
 
+main.add_command(workflow.clean)
 main.add_command(workflow.harvest)
 main.add_command(workflow.process)
 main.add_command(workflow.deposit)
 main.add_command(workflow.postprocess)
-main.add_command(workflow.clean)
