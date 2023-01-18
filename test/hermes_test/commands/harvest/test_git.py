2f672832768142f86d029d2401b68c2824a02992
Stephan Druskat <stephan.druskat@dlr.de>
2023-01-18 15:10:36 +0100

    Modify git harvesting test to account for branch information

--- test/hermes_test/commands/harvest/test_git.py
+++ test/hermes_test/commands/harvest/test_git.py
@@ -81,7 +81,8 @@ def codemeta():
         }
       ]
     }
-  ]
+  ],
+  "hermes:gitBranch": "test-branch"
 }
     """)
 
@@ -93,6 +94,10 @@ Git Author + Committer|e@mail.com|2023-01-13T08:57:12+01:00|Git Author + Committ
 Git Author|f@mail.com|2023-01-13T08:57:12+01:00|Git Committer|g@mail.com|2023-01-13T08:57:12+01:00
     '''
 
+@pytest.fixture
+def git_branch_output():
+    return 'test-branch'
+
 
 @pytest.fixture
 def mock_click_ctx():
@@ -102,7 +107,7 @@ def mock_click_ctx():
 
 
 @patch("hermes.commands.harvest.git.subprocess.run")
-def test_harvest_git(mock_run, mock_click_ctx, harvest_ctx, git_log_output, codemeta):
+def test_harvest_git(mock_run, mock_click_ctx, harvest_ctx, git_log_output, git_branch_output, codemeta):
     # Mocking the output of the subprocess call to git log to retrieve author and committer info
     mock_stdout = Mock()
     mock_stdout.configure_mock(
@@ -114,7 +119,12 @@ def test_harvest_git(mock_run, mock_click_ctx, harvest_ctx, git_log_output, code
 
     # Mocking the output of the subprocess call to git rev-parse to retrieve branch id
     mock_get_branch = Mock()
-    mock_get_branch.returncode = 0
+    mock_get_branch.configure_mock(
+        **{
+            "stdout.decode.return_value": git_branch_output,
+            "returncode": 0
+        }
+    )
 
     # Define outputs from "running" the subprocess mock
     mock_run.side_effect = [mock_get_branch, mock_stdout]
