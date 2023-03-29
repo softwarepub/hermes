9bcb6cbbd23c822c25316de49a8b2dc0a38c718b
Druskat, Stephan <stephan.druskat@dlr.de>
2023-03-29 14:39:14 +0200

    Add failing test for preparing CodeMeta with extra contexts

--- test/hermes_test/model/test_codemeta_context.py
+++ test/hermes_test/model/test_codemeta_context.py
@@ -17,11 +17,51 @@ def mock_ep():
     return ep
 
 
-def test_merge_contexts_from(mock_ep):
-    _self = CodeMetaContext()
-    assert not _self.contexts
+@pytest.fixture
+def _context():
+    return 'foo', 'bar'
+
+
+@pytest.fixture
+def _codemeta_context():
+    return CodeMetaContext()
+
+
+@pytest.fixture
+def _data(_codemeta_context):
+    return {
+        '@context': 'https://doi.org/10.5063/schema/codemeta-2.0',
+        '@type': 'SoftwareSourceCode'
+    }
+
+
+@pytest.fixture
+def _data_with_contexts(_codemeta_context):
+    return {
+        '@type': 'SoftwareSourceCode',
+        '@context': [
+            'https://doi.org/10.5063/schema/codemeta-2.0',
+            {'foo': 'bar'}
+        ]
+    }
+
+
+def test_merge_contexts_from(mock_ep, _context, _codemeta_context):
+    assert not _codemeta_context.contexts
     other = HermesHarvestContext(None, mock_ep)
-    context = ('foo', 'bar')
-    other.contexts.add(context)
-    _self.merge_contexts_from(other)
-    assert _self.contexts == {context}
+    other.contexts.add(_context)
+    _codemeta_context.merge_contexts_from(other)
+    assert _codemeta_context.contexts == {_context}
+
+
+def test_prepare_codemeta(_codemeta_context, _context, _data):
+    assert not _codemeta_context.keys()
+    _codemeta_context.prepare_codemeta()
+    assert _codemeta_context.get_data() == _data
+
+
+def test_prepare_codemeta_with_contexts(_codemeta_context, _context, _data_with_contexts):
+    assert not _codemeta_context.keys()
+    _codemeta_context.contexts = {_context}
+    _codemeta_context.prepare_codemeta()
+    assert _codemeta_context.get_data() == _data_with_contexts
