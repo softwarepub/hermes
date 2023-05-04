5f7ec852251d25acd4cc6280ebb47a5ff6dc95b5
David Pape <d.pape@hzdr.de>
2023-05-04 17:09:08 +0200

    Add tests for access modalities

--- test/hermes_test/commands/deposit/test_invenio.py
+++ test/hermes_test/commands/deposit/test_invenio.py
@@ -3,10 +3,14 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # SPDX-FileContributor: Michael Meinel
+# SPDX-FileContributor: David Pape
+
+from unittest import mock
 
 import pytest
 
 from hermes.commands.deposit import invenio
+from hermes.error import MisconfigurationError
 
 
 def test_resolve_doi(requests_mock):
@@ -62,3 +66,107 @@ def test_resolve_record_id_latest_unknown(requests_mock):
 
     with pytest.raises(ValueError):
         invenio._invenio_resolve_record_id('https://foo.bar', '6789')
+
+
+def test_get_access_modalities_closed():
+    with mock.patch("hermes.config.get") as mocked_deposit_config:
+        mocked_deposit_config.return_value = {
+            'invenio': {
+                'access_right': 'closed',
+            }
+        }
+        access_right, _, _ = invenio._get_access_modalities(None)
+        assert access_right == "closed"
+
+
+def test_get_access_modalities_embargoed_no_date_no_license():
+    with mock.patch("hermes.config.get") as mocked_deposit_config:
+        mocked_deposit_config.return_value = {
+            'invenio': {
+                'access_right': 'embargoed',
+            }
+        }
+        with pytest.raises(MisconfigurationError):
+            invenio._get_access_modalities(None)
+
+
+def test_get_access_modalities_embargoed_no_date_with_license():
+    with mock.patch("hermes.config.get") as mocked_deposit_config:
+        mocked_deposit_config.return_value = {
+            'invenio': {
+                'access_right': 'embargoed',
+            }
+        }
+        with pytest.raises(MisconfigurationError):
+            invenio._get_access_modalities("Apache-2.0")
+
+
+def test_get_access_modalities_embargoed_with_date_with_license():
+    with mock.patch("hermes.config.get") as mocked_deposit_config:
+        mocked_deposit_config.return_value = {
+            'invenio': {
+                'access_right': 'embargoed',
+                'embargo_date': '2050-05-01',
+            }
+        }
+        access_right, embargo_date, _ = invenio._get_access_modalities("Apache-2.0")
+        assert access_right == "embargoed"
+        assert embargo_date == "2050-05-01"
+
+
+def test_get_access_modalities_embargoed_with_broken_date_with_license():
+    with mock.patch("hermes.config.get") as mocked_deposit_config:
+        mocked_deposit_config.return_value = {
+            'invenio': {
+                'access_right': 'embargoed',
+                'embargo_date': 'not-a-date',
+            }
+        }
+        with pytest.raises(MisconfigurationError):
+            invenio._get_access_modalities("Apache-2.0")
+
+
+def test_get_access_modalities_restricted_no_conditions():
+    with mock.patch("hermes.config.get") as mocked_deposit_config:
+        mocked_deposit_config.return_value = {
+            'invenio': {
+                'access_right': 'restricted',
+            }
+        }
+        with pytest.raises(MisconfigurationError):
+            invenio._get_access_modalities(None)
+
+
+def test_get_access_modalities_restricted_with_conditions():
+    with mock.patch("hermes.config.get") as mocked_deposit_config:
+        mocked_deposit_config.return_value = {
+            'invenio': {
+                'access_right': 'restricted',
+                'access_conditions': 'You must be cool'
+            }
+        }
+        access_right, _, access_conditions = invenio._get_access_modalities(None)
+        assert access_right == "restricted"
+        assert access_conditions == "You must be cool"
+
+
+def test_get_access_modalities_open_no_license():
+    with mock.patch("hermes.config.get") as mocked_deposit_config:
+        mocked_deposit_config.return_value = {
+            'invenio': {
+                'access_right': 'open',
+            }
+        }
+        with pytest.raises(MisconfigurationError):
+            invenio._get_access_modalities(None)
+
+
+def test_get_access_modalities_open_with_license():
+    with mock.patch("hermes.config.get") as mocked_deposit_config:
+        mocked_deposit_config.return_value = {
+            'invenio': {
+                'access_right': 'open',
+            }
+        }
+        access_right, _, _ = invenio._get_access_modalities("Apache-2.0")
+        assert access_right == "open"
