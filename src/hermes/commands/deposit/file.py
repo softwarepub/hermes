d1dd493bdb0f8695ae2418bcb83dd4aca8d9fd5a
Druskat, Stephan <stephan.druskat@dlr.de>
2023-11-15 16:58:32 +0100

    Remove click from file depositor

--- src/hermes/commands/deposit/file.py
+++ src/hermes/commands/deposit/file.py
@@ -12,17 +12,17 @@ from hermes.model.context import CodeMetaContext
 from hermes.model.path import ContextPath
 
 
-def dummy_noop(click_ctx: click.Context, ctx: CodeMetaContext):
+def dummy_noop(ctx: CodeMetaContext):
     pass
 
 
-def map_metadata(click_ctx: click.Context, ctx: CodeMetaContext):
-    ctx.update(ContextPath.parse('deposit.file'), ctx['codemeta'])
+def map_metadata(ctx: CodeMetaContext):
+    ctx.update(ContextPath.parse("deposit.file"), ctx["codemeta"])
 
 
-def publish(click_ctx: click.Context, ctx: CodeMetaContext):
+def publish(ctx: CodeMetaContext):
     file_config = config.get("deposit").get("file", {})
-    output_data = ctx['deposit.file']
+    output_data = ctx["deposit.file"]
 
-    with open(file_config.get('filename', 'hermes.json'), 'w') as deposition_file:
+    with open(file_config.get("filename", "hermes.json"), "w") as deposition_file:
         json.dump(output_data, deposition_file)
