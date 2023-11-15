a9443e51931258c6d33168f4bbb90b3d863f9b46
Druskat, Stephan <stephan.druskat@dlr.de>
2023-11-15 17:23:23 +0100

    Adapt postprocessors to use new signatures

--- src/hermes/commands/postprocess/invenio_rdm.py
+++ src/hermes/commands/postprocess/invenio_rdm.py
@@ -7,22 +7,32 @@
 
 import json
 import logging
+from pathlib import Path
 
 import toml
 
 from hermes import config
 
 
-_log = logging.getLogger('deposit.invenio_rdm')
+_log = logging.getLogger("deposit.invenio_rdm")
 
 
-def config_record_id(ctx):
-    deposition_path = ctx.get_cache('deposit', 'deposit')
+def config_record_id(
+    path: Path,
+    config_path: Path,
+    initial: bool,
+    auth_token: str,
+    files: list[Path],
+    ctx,
+):
+    deposition_path = ctx.get_cache("deposit", "deposit")
     with deposition_path.open("r") as deposition_file:
         deposition = json.load(deposition_file)
-    conf = config.get('hermes')
+    conf = config.get("hermes")
     try:
-        conf['deposit']['invenio_rdm']['record_id'] = deposition['record_id']
-        toml.dump(conf, open('hermes.toml', 'w'))
+        conf["deposit"]["invenio_rdm"]["record_id"] = deposition["record_id"]
+        toml.dump(conf, open("hermes.toml", "w"))
     except KeyError:
-        raise RuntimeError("No deposit.invenio configuration available to store record id in") from None
+        raise RuntimeError(
+            "No deposit.invenio configuration available to store record id in"
+        ) from None
