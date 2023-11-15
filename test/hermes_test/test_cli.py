32f7f6f6ce9aa70db9e39b0ce0e59eb46e6039e7
Druskat, Stephan <stephan.druskat@dlr.de>
2023-11-15 17:34:02 +0100

    Remove unneeded click-based tests

--- test/hermes_test/test_cli.py
+++ test/hermes_test/test_cli.py
@@ -8,71 +8,9 @@ import pathlib
 from unittest import mock
 import pytest
 
-import click
-from click.testing import CliRunner
-
 from hermes import cli
 from hermes.commands.deposit.error import DepositionUnauthorizedError
 from hermes_test.mocks import mock_command
-
-
-def test_workflow_setup():
-    wf = cli.WorkflowCommand()
-
-    wf.add_command(mock_command("spam")[1])
-    wf.add_command(mock_command("eggs")[1])
-
-    ctx = click.Context(wf)
-    assert wf.list_commands(ctx) == ["spam", "eggs"]
-
-
-def test_workflow_no_dupes():
-    wf = cli.WorkflowCommand()
-
-    wf.add_command(mock_command("spam")[1])
-    wf.add_command(mock_command("eggs")[1])
-    wf.add_command(mock_command("ham")[1], "spam")
-
-    ctx = click.Context(wf)
-    assert wf.list_commands(ctx) == ["spam", "eggs"]
-    assert wf.get_command(ctx, "spam").name == "ham"
-
-
-def test_workflow_invoke():
-    wf = cli.WorkflowCommand()
-    spam, spam_cmd = mock_command("spam")
-    eggs, eggs_cmd = mock_command("eggs")
-
-    wf.add_command(spam_cmd)
-    wf.add_command(eggs_cmd)
-
-    ctx = click.Context(wf)
-    ctx.params['path'] = pathlib.Path.cwd()
-    ctx.params['config'] = pathlib.Path.cwd() / 'hermes.toml'
-    wf.invoke(ctx)
-
-    spam.assert_called_once()
-    eggs.assert_called_once()
-
-
-def test_workflow_invoke_with_cb():
-    wf = cli.WorkflowCommand()
-    cb_mock = mock.Mock()
-    spam, spam_cmd = mock_command("spam")
-    eggs, eggs_cmd = mock_command("eggs")
-
-    wf.add_command(spam_cmd)
-    wf.add_command(eggs_cmd)
-    wf.result_callback()(cb_mock)
-
-    ctx = click.Context(wf)
-    ctx.params['path'] = pathlib.Path.cwd()
-    ctx.params['config'] = pathlib.Path.cwd() / 'hermes.toml'
-    wf.invoke(ctx)
-
-    spam.assert_called_once()
-    eggs.assert_called_once()
-    cb_mock.assert_called_with(["spam", "eggs"], config=ctx.params['config'], path=ctx.params['path'])
 
 
 def test_hermes_full():
@@ -84,72 +22,84 @@ def test_hermes_full():
 
 def test_hermes_harvest():
     runner = CliRunner()
-    result = runner.invoke(cli.main, args=('harvest', ))
+    result = runner.invoke(cli.main, args=("harvest",))
 
     assert not result.exception
 
 
 def test_hermes_process():
     runner = CliRunner()
-    result = runner.invoke(cli.main, args=('process',))
+    result = runner.invoke(cli.main, args=("process",))
 
     assert not result.exception
 
 
-@pytest.mark.skip(reason="No clean way at the moment of adding more required options that are parsed by Click, \
-                         e.g. files args or options")
+@pytest.mark.skip(
+    reason="No clean way at the moment of adding more required options that are parsed by Click, \
+                         e.g. files args or options"
+)
 def test_hermes_with_deposit():
     runner = CliRunner()
-    result = runner.invoke(cli.main, args=('--deposit', ))
+    result = runner.invoke(cli.main, args=("--deposit",))
 
     assert isinstance(result.exception, DepositionUnauthorizedError)
 
 
-@pytest.mark.skip(reason="No clean way at the moment of adding more required options that are parsed by Click, \
-                         e.g. files args or options")
+@pytest.mark.skip(
+    reason="No clean way at the moment of adding more required options that are parsed by Click, \
+                         e.g. files args or options"
+)
 def test_hermes_with_postprocess():
     runner = CliRunner()
-    result = runner.invoke(cli.main, args=('--postprocess', ))
+    result = runner.invoke(cli.main, args=("--postprocess",))
 
     assert not result.exception
 
 
 def test_hermes_with_path():
     runner = CliRunner()
-    result = runner.invoke(cli.main, args=('--path', './'))
+    result = runner.invoke(cli.main, args=("--path", "./"))
 
     assert not result.exception
 
 
-@pytest.mark.skip(reason="No clean way at the moment of adding more required options that are parsed by Click, \
-                         e.g. files args or options")
+@pytest.mark.skip(
+    reason="No clean way at the moment of adding more required options that are parsed by Click, \
+                         e.g. files args or options"
+)
 def test_hermes_with_deposit_and_postprocess():
     runner = CliRunner()
-    result = runner.invoke(cli.main, args=('--deposit', '--postprocess'))
+    result = runner.invoke(cli.main, args=("--deposit", "--postprocess"))
 
     assert isinstance(result.exception, DepositionUnauthorizedError)
 
 
-@pytest.mark.skip(reason="No clean way at the moment of adding more required options that are parsed by Click, \
-                         e.g. files args or options")
+@pytest.mark.skip(
+    reason="No clean way at the moment of adding more required options that are parsed by Click, \
+                         e.g. files args or options"
+)
 def test_hermes_with_deposit_and_path():
     runner = CliRunner()
-    result = runner.invoke(cli.main, args=('--deposit', '--path', './'))
+    result = runner.invoke(cli.main, args=("--deposit", "--path", "./"))
 
     assert result.exit_code == 2
 
 
 def test_hermes_with_path_and_postprocess():
     runner = CliRunner()
-    result = runner.invoke(cli.main, args=('--path', './', '--postprocess'))
+    result = runner.invoke(cli.main, args=("--path", "./", "--postprocess"))
 
     assert result.exit_code == 1
 
 
-@pytest.mark.skip(reason="No clean way at the moment of adding more required options that are parsed by Click, \
-                         e.g. files args or options")
+@pytest.mark.skip(
+    reason="No clean way at the moment of adding more required options that are parsed by Click, \
+                         e.g. files args or options"
+)
 def test_hermes_with_deposit_and_postprocess_and_path():
     runner = CliRunner()
-    result = runner.invoke(cli.main, args=('--deposit', '--postprocess', '--path', './'))
+    result = runner.invoke(
+        cli.main, args=("--deposit", "--postprocess", "--path", "./")
+    )
 
     assert isinstance(result.exception, DepositionUnauthorizedError)
