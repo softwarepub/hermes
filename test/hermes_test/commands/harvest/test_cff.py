4dcfc2b2ae19deae33094f181bb6223651207ed8
Druskat, Stephan <stephan.druskat@dlr.de>
2023-12-13 10:03:13 +0100

    Adapt tests to new patch API

--- test/hermes_test/commands/harvest/test_cff.py
+++ test/hermes_test/commands/harvest/test_cff.py
@@ -69,29 +69,33 @@ def test_validate_fail():
 # Regression test for https://github.com/hermes-hmc/workflow/issues/112
 
 @pytest.fixture
-def codemeta_with_email():
-    return json.loads("""{
+def codemeta_without_email():
+    return {
       "@context": "https://doi.org/10.5063/schema/codemeta-2.0",
       "@type": "SoftwareSourceCode",
       "author": [
         {
           "@type": "Person",
-          "email": "email@example.com",
           "familyName": "Author"
         },
         {
           "@type": "Person",
-          "email": "email2@example.com",
           "familyName": "Second"
         },
         {
           "@type": "Person",
-          "email": "email3@example.com",
           "familyName": "Third"
         }
       ],
       "name": "Title"
-    }""")
+    }
+
+@pytest.fixture
+def codemeta_with_email(codemeta_without_email):
+    author_emails = {"Author": "email@example.com", "Second": "email2@example.com", "Third": "email3@example.com",}
+    for author in codemeta_without_email["author"]:
+        author["email"] = author_emails[author["familyName"]]
+    return codemeta_without_email
 
 
 @pytest.fixture
@@ -109,12 +113,9 @@ def valid_minimal_cff_with_email(tmp_path):
     title: Title
     """
     yaml = YAML()
-    cff_yml = yaml.load(cff)
-    cff_file = tmp_path / 'CITATION.cff'
-    yaml.dump(cff_yml, cff_file)
-    return cff_file
+    return yaml.load(cff)
 
 
 def test_convert_cff_to_codemeta_with_email(valid_minimal_cff_with_email, codemeta_with_email):
-    actual_result = harvest._convert_cff_to_codemeta(valid_minimal_cff_with_email.read_text())
+    actual_result = harvest._patch_author_emails(valid_minimal_cff_with_email, codemeta_with_email)
     assert codemeta_with_email == actual_result
