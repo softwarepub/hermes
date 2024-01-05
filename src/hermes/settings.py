76a5d94e1730a70d0ea5a7fbaf8d81b691b4f090
Kernchen, Sophie <sophie.kernchen@dlr.de>
2024-01-05 07:39:17 +0100

    Set up Settings to read toml

--- src/hermes/settings.py
+++ src/hermes/settings.py
@@ -1,13 +1,14 @@
-# SPDX-FileCopyrightText: 2023 German Aerospace Center (DLR)
-#
-# SPDX-License-Identifier: Apache-2.0
-
-# SPDX-FileContributor: Sophie Kernchen
-
 from pydantic import BaseModel
+import toml
+import pathlib
+from typing import Any, Dict, Tuple, Type, ClassVar
+
+from pydantic.fields import FieldInfo
+
 
 from pydantic_settings import (
     BaseSettings,
+    PydanticBaseSettingsSource,
     SettingsConfigDict,
 )
 
@@ -33,11 +34,76 @@ class DepositSettings(BaseModel):
     invenio_rdm: DepositTargetSettings = DepositTargetSettings
 
 
-class HermesSettings(BaseModel):
+class HermesSettings(BaseSettings):
+
+    model_config = SettingsConfigDict(env_file_encoding='utf-8', extra='allow')
+
     harvest: HarvestSettings = HarvestSettings
     deposit: DepositSettings = DepositSettings
+    config_path: ClassVar[pathlib.Path] = "hermes.toml" #TODO : Set config Path in cli
+
+    def __init__(self, conf_path):
+        super().__init__()
+        self.__class__.config_path = conf_path
+
+    @classmethod
+    def settings_customise_sources(
+            cls,
+            settings_cls: Type[BaseSettings],
+            init_settings: PydanticBaseSettingsSource,
+            env_settings: PydanticBaseSettingsSource,
+            dotenv_settings: PydanticBaseSettingsSource,
+            file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        return (
+            init_settings,
+            TomlConfigSettingsSource(settings_cls, cls.config_path),
+            env_settings,
+            file_secret_settings,
+        )
+
+    def __getattr__(self, item):
+        self.__pydantic_extra__ = {item: {}}
+        return item
 
 
-class Settings(BaseSettings):
-    hermes: HermesSettings = HermesSettings
-    model_config = SettingsConfigDict(env_prefix='hermes_')
+class TomlConfigSettingsSource(PydanticBaseSettingsSource):
+    """
+    A simple settings source class that loads variables from a TOML file
+    at the project's root.
+
+    Here we happen to choose to use the `env_file_encoding` from Config
+    when reading `config.json`
+    """
+    def __init__(self, settings_cls, config_path):
+        super().__init__(settings_cls)
+        self.__config_path = config_path
+
+    def get_field_value(
+        self, field: FieldInfo, field_name: str
+    ) -> Tuple[Any, str, bool]:
+        encoding = self.config.get('env_file_encoding')
+        file_content_toml = toml.load(
+            pathlib.Path(self.__config_path)
+        )
+        field_value = file_content_toml.get(field_name)
+        return field_value, field_name, False
+
+    def prepare_field_value(
+        self, field_name: str, field: FieldInfo, value: Any, value_is_complex: bool
+    ) -> Any:
+        return value
+
+    def __call__(self) -> Dict[str, Any]:
+        d: Dict[str, Any] = {}
+        for field_name, field in self.settings_cls.model_fields.items():
+            field_value, field_key, value_is_complex = self.get_field_value(
+                field, field_name
+            )
+            field_value = self.prepare_field_value(
+                field_name, field, field_value, value_is_complex
+            )
+            if field_value is not None:
+                d[field_key] = field_value
+
+        return d
