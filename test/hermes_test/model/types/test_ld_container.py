4e45698096610cfa68e7813627f6ef3d99caec95
Kernchen, Sophie <sophie.kernchen@dlr.de>
2025-07-11 14:11:59 +0200

    First test for ld_container

---
+++ test/hermes_test/model/types/test_ld_container.py
@@ -0,0 +1,19 @@
+from hermes.model.types.ld_container import ld_container
+
+'''we expect user of this class to give the right input data types
+
+example extendeed json ld:
+        {
+      "http://schema.org/name": [{"@value": "bacon"}],
+      "eggs": [{"@id": "spam"}],
+      "green": [{"@id": "png"}]
+        }
+'''
+
+
+def test_container_basic():
+    cont = ld_container({"spam": [{"@value": "bacon"}]})
+
+    assert cont.key is None
+    assert cont.context == []
+    assert cont._data == {"spam": [{"@value": "bacon"}]}
