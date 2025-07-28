55ad4757c46d674b2c97be56aff450c9db98c44e
Stephan Druskat <stephan.druskat@dlr.de>
2025-07-28 07:51:07 +0200

    Add documentation and some FIXMEs

--- src/hermes/model/types/ld_context.py
+++ src/hermes/model/types/ld_context.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # SPDX-FileContributor: Michael Meinel
+# SPDX-FileContributor: Stephan Druskat <stephan.druskat@dlr.de>
 
 
 CODEMETA_PREFIX = "https://doi.org/10.5063/schema/codemeta-2.0"
@@ -27,7 +28,20 @@ ALL_CONTEXTS = [*CODEMETA_CONTEXT, {**SCHEMA_ORG_CONTEXT[0], **PROV_CONTEXT[0], 
 
 
 class ContextPrefix:
+    """
+    FIXME: Rename to `LDContext`, `HermesLDContext` or similar,
+    FIXME: as this class represents JSON-LD contexts.
+    Represents the context of the hermes JSON-LD data model and provides two views on the model:
+
+    - as a two-item list, of which the first item is the default, unprefixed vocabulary IRI, and the second is a dict
+    mapping prefixes to vocabulary IRIs;
+    - as a dict mapping prefixes to vocabulary IRIs, where the default vocabulary has a prefix of None.
+    """
     def __init__(self, context):
+        """
+        @param context: A two-item list, where the first item is the default vocabulary's IRI string, and the second
+        is a dict mapping vocabulary prefixes to their respective IRI string.
+        """
         self.context = context
         self.prefix = {}
 
@@ -41,7 +55,15 @@ class ContextPrefix:
                 if isinstance(base_url, str)
             })
 
-    def __getitem__(self, item):
+
+    def __getitem__(self, item):  # FIXME Rename parameter to 'key'
+        """
+        FIXME: Document in class, not here
+        FIXME: Add type hints for params and return
+
+        Gets the fully qualified IRI for a term from a vocabulary inside the initialized context.
+        The vocabulary must have been added to the context at initialization.
+        """
         if not isinstance(item, str):
             prefix, name = item
         elif ':' in item:
