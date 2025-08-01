a527fb0a201497e0e8dbdd238973fb7250016322
Michael Meinel <led02@me.com>
2025-08-01 21:11:37 +0200

    Add test for helpers.

---
+++ test/hermes_test/model/types/test_pyld_util.py
@@ -0,0 +1,18 @@
+import pytest
+
+from hermes.model.types import pyld_util
+
+
+@pytest.fixture
+def ld_proc():
+    return pyld_util.JsonLdProcessor()
+
+
+def test_mock_document_compact(ld_proc, mock_document):
+    compact_document = ld_proc.compact(mock_document.expanded(), [mock_document.vocabulary], {})
+    assert compact_document == mock_document.compact()
+
+
+def test_mock_document_compact(ld_proc, mock_document):
+    expanded_document = ld_proc.expand(mock_document.compact(), {})
+    assert expanded_document == mock_document.expanded()
