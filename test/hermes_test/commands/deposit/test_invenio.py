b347dc22f4347eb5b90bdeee1e435128d5a03b32
Michael Meinel <led02@me.com>
2025-08-01 22:37:36 +0200

    Silence flake8 on inactive tests.

--- test/hermes_test/commands/deposit/test_invenio.py
+++ test/hermes_test/commands/deposit/test_invenio.py
@@ -12,6 +12,7 @@ import pytest
 
 pytest.skip("FIXME: Re-enable test after data model refactoring is done.", allow_module_level=True)
 
+# noqa: E402
 from hermes.commands.deposit import invenio
 from hermes.error import MisconfigurationError
 
--- test/hermes_test/test_cli.py
+++ test/hermes_test/test_cli.py
@@ -8,6 +8,7 @@ import pytest
 
 pytest.skip("FIXME: Re-enable test after data model refactoring is done.", allow_module_level=True)
 
+# noqa: E402
 from hermes.commands import cli
 
 
