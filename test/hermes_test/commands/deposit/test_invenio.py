7a2b0d63a28fa18d0b4e219eb4a3878b559ce1ce
Michael Meinel <led02@me.com>
2025-08-01 22:39:52 +0200

    Silence flake8 on inactive tests more agressively.

--- test/hermes_test/commands/deposit/test_invenio.py
+++ test/hermes_test/commands/deposit/test_invenio.py
@@ -5,6 +5,8 @@
 # SPDX-FileContributor: Michael Meinel
 # SPDX-FileContributor: David Pape
 
+# flake8: noqa
+
 from unittest import mock
 
 import click
@@ -12,7 +14,6 @@ import pytest
 
 pytest.skip("FIXME: Re-enable test after data model refactoring is done.", allow_module_level=True)
 
-# noqa: E402
 from hermes.commands.deposit import invenio
 from hermes.error import MisconfigurationError
 
