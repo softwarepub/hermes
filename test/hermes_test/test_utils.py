8bf634383f6de70d86f8201a069613a9c63be5cd
Stephan Druskat <stephan.druskat@dlr.de>
2025-08-01 13:26:58 +0200

    Use distribution version in test

--- test/hermes_test/test_utils.py
+++ test/hermes_test/test_utils.py
@@ -1,13 +1,16 @@
-from importlib.resources import files
-import toml
+# SPDX-FileCopyrightText: 2025 German Aerospace Center (DLR)
+# SPDX-FileContributor: Stephan Druskat <stephan.druskat@dlr.de>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from importlib import metadata
 
 from hermes.utils import hermes_user_agent
 
-pyproject = toml.loads(files("hermes").joinpath("../../pyproject.toml").read_text())
-name = pyproject["project"]["name"]
-version = pyproject["project"]["version"]
-homepage = pyproject["project"]["urls"]["homepage"]
+expected_name = "hermes"
+expected_homepage = "https://hermes.software-metadata.pub"
+dist_version = metadata.version("hermes")
 
 
 def test_hermes_user_agent():
-    assert hermes_user_agent == f"{name}/{version} ({homepage})"
+    assert hermes_user_agent == f"{expected_name}/{dist_version} ({expected_homepage})"
