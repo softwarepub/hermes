2679a0a44cdd9a3cd593fbeaf4ccbb84cefa2c03
notactuallyfinn <michael.fritzsche@dlr.de>
2025-09-05 13:53:59 +0200

    added more tests

--- test/hermes_test/model/types/test_ld_list.py
+++ test/hermes_test/model/types/test_ld_list.py
@@ -54,3 +54,73 @@ def test_build_in_set():
         li[::2] = "foo"
     with pytest.raises(TypeError):
         li[:2] = 1
+
+
+def test_build_in_len():
+    assert len(ld_list([{"@list": []}])) == 0
+    assert len(ld_list([{"@list": [{"@value": "foo"}, {"@value": "bar"}, {"@value": "foobar"}]}])) == 3
+
+
+def test_build_in_iter():
+    li = ld_list([{"@list": [{"@value": "foo"}, ld_dict([{"@type": "A", "schema:name": "a"}])]}],
+                 key="https://schema.org/name", context={"schema": "https://schema.org/"})
+    li = [val for val in li]
+    assert li[0] == "foo" and li[1].data_dict == {"@type": "A", "schema:name": "a"} and li[1].index == 1
+
+
+def test_append():
+    li = ld_list([{"@list": []}], key="https://schema.org/name", context={"schema": "https://schema.org/"})
+    li.append("foo")
+    assert li[0] == "foo" and li.item_list[0] == {"@value": "foo"} and len(li) == 1
+    li.append("bar")
+    assert li[0:2] == ["foo", "bar"] and li.item_list[1] == {"@value": "bar"} and len(li) == 2
+    li.append(ld_dict([{"@type": "A", "schema:name": "a"}]))
+    assert li[-1].data_dict == {"@type": "A", "schema:name": "a"} and len(li) == 3
+
+
+def test_extend():
+    li = ld_list([{"@list": []}], key="https://schema.org/name", context={"schema": "https://schema.org/"})
+    li.extend([])
+    assert len(li) == 0
+    li.extend(["foo"])
+    assert li[0] == "foo" and li.item_list[0] == {"@value": "foo"} and len(li) == 1
+    li.extend(["bar"])
+    assert li[0:2] == ["foo", "bar"] and li.item_list[1] == {"@value": "bar"} and len(li) == 2
+    li.extend([ld_dict([{"@type": "A", "schema:name": "a"}])])
+    assert li[-1].data_dict == {"@type": "A", "schema:name": "a"} and len(li) == 3
+
+    li = ld_list([{"@list": []}], key="https://schema.org/name", context={"schema": "https://schema.org/"})
+    li.extend(["foo", "bar", ld_dict([{"@type": "A", "schema:name": "a"}])])
+    assert li[0:2] == ["foo", "bar"] and li.item_list[0:2] == [{"@value": "foo"}, {"@value": "bar"}]
+    assert li[-1].data_dict == {"@type": "A", "schema:name": "a"} and len(li) == 3
+
+    li = ld_list([{"@list": []}], key="https://schema.org/name", context={"schema": "https://schema.org/"})
+    li.append("foo")
+    li.extend(["bar", ld_dict([{"@type": "A", "schema:name": "a"}])])
+    assert li[0:2] == ["foo", "bar"] and li.item_list[0:2] == [{"@value": "foo"}, {"@value": "bar"}]
+    assert li[-1].data_dict == {"@type": "A", "schema:name": "a"} and len(li) == 3
+
+
+def test_is_ld_list():
+    assert not any(ld_list.is_ld_list(item) for item in [1, "", [], {}, {"@list": []}, [{}], [{"a": "b"}]])
+    assert not any(ld_list.is_ld_list(item) for item in [[{"@list": ""}], [{"@list": ["a"]}, {"@list": ["b"]}]])
+    assert all(ld_list.is_ld_list([{container_type: []}]) for container_type in ["@list", "@set", "@graph"])
+
+
+def test_is_container():
+    assert not any(ld_list.is_container(item) for item in [1, "", [], {}, {"a": "b"}])
+    assert not any(ld_list.is_container(item) for item in [ld_dict([{"a": "b"}]), ld_list([{"@list": ["a"]}])])
+    assert not any(ld_list.is_container({"@list": value}) for value in ["", 1, {}])
+    assert all(ld_list.is_container({container_type: []}) for container_type in ["@list", "@graph", "@set"])
+
+
+def test_from_list():
+    li = ld_list.from_list([])
+    assert li.container == "@list" and li.item_list == li.context == [] and li.parent is li.key is li.index is None
+    li = ld_list.from_list([], parent=li, key="schema:name", context={"schema":"https://schema.org/"}, container="@set")
+    assert li.container == "@set" and li.item_list == [] and li.parent is not None and li.key == "schema:name"
+    assert li.index is None and li.context == {"schema":"https://schema.org/"}
+    li = ld_list.from_list(["a", {"@value": "b"}], parent=None, key="https://schema.org/name",
+                           context={"schema":"https://schema.org/"}, container="@graph")
+    assert li.container == "@graph" and li.item_list == [{"@value": "a"}, {"@value": "b"}] and li.parent is None
+    assert li.key == "https://schema.org/name" and li.index is None and li.context == {"schema":"https://schema.org/"}
