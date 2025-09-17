69f6a245dd1b3c52c4a77fd62a30de8c73ee02a2
Stephan Druskat <stephan.druskat@dlr.de>
2025-09-17 18:09:03 +0200

    Test API object initialization with nested object

--- test/hermes_test/model/test_api.py
+++ test/hermes_test/model/test_api.py
@@ -6,6 +6,7 @@ from hermes.model.types.ld_context import ALL_CONTEXTS
 
 EXTRA_VOCABS = {"foo": "https://bar.net/schema"}
 
+
 @pytest.fixture
 def default_context():
     return {"@context": ALL_CONTEXTS}
@@ -14,12 +15,14 @@ def default_context():
 def custom_context():
     return {"@context": ALL_CONTEXTS + [EXTRA_VOCABS]}
 
+
 @pytest.mark.parametrize("metadata,full_context", [
     (SoftwareMetadata(), "default_context"),
     (SoftwareMetadata(extra_vocabs=EXTRA_VOCABS), "custom_context"),
 ])
 def test_init_no_data(metadata, full_context, request):
     assert metadata.full_context == request.getfixturevalue(full_context)["@context"]
+
 
 @pytest.mark.parametrize("metadata,full_context", [
     (SoftwareMetadata({"funding": "foo"}), "default_context"),
@@ -28,3 +31,14 @@ def test_init_no_data(metadata, full_context, request):
 def test_init_with_data(metadata, full_context, request):
     assert metadata.full_context == request.getfixturevalue(full_context)["@context"]
     assert metadata["funding"] == "foo"
+
+
+def test_init_full_object():
+    my_software = {"foo:softwareName": "MySoftware", "foo:egg": "spam", "foo:ham": "eggs",
+                   "maintainer": {"name": "Some Name", "email": "maintainer@example.com"},
+                   "author": [{"name": "Foo"}, {"name": "Bar"}]}
+    data = SoftwareMetadata(my_software, extra_vocabs={"foo": "https://foo.bar"})
+    assert data["foo:softwareName"] == "MySoftware"
+    assert data["maintainer"]["name"] == "Some Name"
+    for author in data["author"]:
+        assert author["name"] in ["Foo", "Bar"]
