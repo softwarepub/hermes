79575b8845123c4aabc8b35b6d6ab511f9dfb81f
Stephan Druskat <stephan.druskat@dlr.de>
2025-09-17 18:03:51 +0200

    Test API object initiatlization with and without data

--- test/hermes_test/model/test_api.py
+++ test/hermes_test/model/test_api.py
@@ -11,29 +11,20 @@ def default_context():
     return {"@context": ALL_CONTEXTS}
 
 @pytest.fixture
-def default_ld():
-    return {"@context": ALL_CONTEXTS, "funding": "foo"}
-
-@pytest.fixture
 def custom_context():
     return {"@context": ALL_CONTEXTS + [EXTRA_VOCABS]}
 
-@pytest.fixture
-def custom_ld():
-    return {"@context": ALL_CONTEXTS + [EXTRA_VOCABS], "funding": "foo"}
-
-@pytest.fixture
-def none():
-    return None
-
-@pytest.mark.parametrize("data,codemeta,full_context,expanded", [
-    (SoftwareMetadata(), "default_context", "default_context", "none"),  # FIXME: Replace none fixtures
-    (SoftwareMetadata({"funding": "foo"}), "default_ld", "none", "none"),  # FIXME: Replace none fixtures
-    (SoftwareMetadata(extra_vocabs=EXTRA_VOCABS), "custom_context", "custom_context", "none"),  # FIXME: Replace none fixtures
-    (SoftwareMetadata({"funding": "foo"}, extra_vocabs=EXTRA_VOCABS), "custom_ld", "none", "none"),  # FIXME: Replace none fixtures
+@pytest.mark.parametrize("metadata,full_context", [
+    (SoftwareMetadata(), "default_context"),
+    (SoftwareMetadata(extra_vocabs=EXTRA_VOCABS), "custom_context"),
 ])
-def test_init(data, codemeta, full_context, expanded, request):
-    assert data.compact() == request.getfixturevalue(codemeta)
-    assert data.full_context == request.getfixturevalue(full_context)["@context"]
-    assert data.ld_value == request.getfixturevalue(expanded)
+def test_init_no_data(metadata, full_context, request):
+    assert metadata.full_context == request.getfixturevalue(full_context)["@context"]
 
+@pytest.mark.parametrize("metadata,full_context", [
+    (SoftwareMetadata({"funding": "foo"}), "default_context"),
+    (SoftwareMetadata({"funding": "foo"}, extra_vocabs=EXTRA_VOCABS), "custom_context"),
+])
+def test_init_with_data(metadata, full_context, request):
+    assert metadata.full_context == request.getfixturevalue(full_context)["@context"]
+    assert metadata["funding"] == "foo"
