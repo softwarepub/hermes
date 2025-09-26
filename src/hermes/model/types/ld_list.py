0f324949c8be3f90cd1d5bc5c705b473e35ec055
notactuallyfinn <michael.fritzsche@dlr.de>
2025-09-26 12:46:05 +0200

    changed conversions of types to output ld_lists for every item in a dict

--- src/hermes/model/types/ld_list.py
+++ src/hermes/model/types/ld_list.py
@@ -33,7 +33,7 @@ class ld_list(ld_container):
         if isinstance(index, slice):
             return [self[i] for i in [*range(len(self))][index]]
 
-        item = self._to_python(self.key, self.item_list[index:index + 1])
+        item = self._to_python(self.key, self.item_list[index])
         if isinstance(item, ld_container):
             item.index = index
         return item
@@ -46,7 +46,7 @@ class ld_list(ld_container):
 
     def __iter__(self):
         for index, value in enumerate(self.item_list):
-            item = self._to_python(self.key, [value])
+            item = self._to_python(self.key, value)
             if isinstance(item, ld_container):
                 item.index = index
             yield item
