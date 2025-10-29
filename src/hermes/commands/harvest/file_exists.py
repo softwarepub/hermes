78ba4c9e06c0e5213b26f5b897fce84f307e5d65
David Pape <d.pape@hzdr.de>
2025-10-29 14:58:23 +0100

    Implement file tagging based on pattern group

--- src/hermes/commands/harvest/file_exists.py
+++ src/hermes/commands/harvest/file_exists.py
@@ -2,11 +2,12 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileContributor: David Pape
 
+from collections import defaultdict
 from dataclasses import dataclass
 from functools import cache
 from mimetypes import guess_type
 from pathlib import Path
-from typing import List, Optional
+from typing import Dict, List, Optional, Set
 from typing_extensions import Self
 import subprocess
 
@@ -56,17 +57,19 @@ class TextObject:
 class CreativeWork:
     name: str
     associated_media: TextObject
+    keywords: List[str]
 
     @classmethod
-    def from_path(cls, path: Path) -> Self:
+    def from_path(cls, path: Path, keywords: List[str]) -> Self:
         text_object = TextObject.from_path(path)
-        return cls(name=path.stem, associated_media=text_object)
+        return cls(name=path.stem, associated_media=text_object, keywords=keywords)
 
     def as_codemeta(self) -> dict:
         return {
             "@type": "schema:CreativeWork",
             "schema:name": self.name,
             "schema:associatedMedia": self.associated_media.as_codemeta(),
+            "schema:keywords": self.keywords,
         }
 
 
@@ -74,88 +77,99 @@ class FileExistsHarvestSettings(BaseModel):
     """Settings for ``file_exists`` harvester."""
 
     enable_git_ls_files: bool = True
+    search_patterns: Dict[str, List[str]] = {}
 
 
 class FileExistsHarvestPlugin(HermesHarvestPlugin):
     settings_class = FileExistsHarvestSettings
 
-    search_patterns = [
-        "contributing",
-        "contributing.md",
-        "contributing.txt",
-    ]
-    search_patterns_license = [
-        "license",
-        "license.txt",
-        "license.md",
-        "licenses/*.txt",
-    ]
-    search_patterns_readme = [
-        "readme",
-        "readme.md",
-        "readme.markdown",
-        "readme.rst",
-        "readme.txt",
-    ]
+    base_search_patterns = {
+        "readme": [
+            "readme",
+            "readme.md",
+            "readme.markdown",
+            "readme.rst",
+            "readme.txt",
+        ],
+        "license": [
+            "license",
+            "license.txt",
+            "license.md",
+            "licenses/*.txt",
+        ],
+    }
 
     def __init__(self):
         self.working_directory: Path = Path.cwd()
         self.settings: FileExistsHarvestSettings = FileExistsHarvestSettings()
 
+        # mapping from tag name to list of file name patterns
+        self.search_patterns: Dict[str, List[str]] = self.base_search_patterns
+        # mapping from file name pattern to list of tags
+        self.search_pattern_keywords: Dict[str, List[str]] = defaultdict(list)
+        # flat list of file name patterns
+        self.search_pattern_list: List[str] = []
+
     def __call__(self, command: HermesHarvestCommand):
         self.working_directory = command.args.path.resolve()
         self.settings = command.settings.file_exists
 
-        license_files = self._find_files(self.search_patterns_license)
-        readme_files = self._find_files(self.search_patterns_readme)
-        other_files = self._find_files(self.search_patterns)
+        # update search patterns from config
+        self.search_patterns.update(self.settings.search_patterns)
 
+        # create inverse lookup table
+        for key, patterns in self.search_patterns.items():
+            for pattern in patterns:
+                self.search_pattern_keywords[pattern].append(key)
+
+        self.search_pattern_list = sum(self.search_patterns.values(), start=[])
+
+        files = self._find_files()
         data = {
-            "schema:hasPart": [
-                file.as_codemeta()
-                for file in license_files + readme_files + other_files
-            ],
+            "schema:hasPart": [file.as_codemeta() for file in files],
             "schema:license": [
-                file.associated_media.url.as_codemeta() for file in license_files
+                file.associated_media.url.as_codemeta()
+                for file in files
+                if file.keywords and "license" in file.keywords
             ],
             "codemeta:readme": [
-                file.associated_media.url.as_codemeta() for file in readme_files
+                file.associated_media.url.as_codemeta()
+                for file in files
+                if file.keywords and "readme" in file.keywords
             ],
         }
 
         return data, {"workingDirectory": str(self.working_directory)}
 
-    def _find_files(self, file_name_patterns: List[str]) -> List[CreativeWork]:
-        """Find files that match ``file_name_patterns``.
+    def _find_files(self) -> List[CreativeWork]:
+        """Find files that match the search patterns.
 
         If the setting ``enable_git_ls_files`` is ``True``, ``git ls-files`` is used to
         find matching files. If it is set to ``False`` or getting the list from git
         fails, the working directory is searched recursively.
+
+        The files are tagged using the search pattern "groups" as the keywords.
         """
         files = None
         if self.settings.enable_git_ls_files:
-            files = self._find_files_git(file_name_patterns)
+            files = _git_ls_files(self.working_directory)
         if files is None:
-            files = self._find_files_directory(file_name_patterns)
-        return [CreativeWork.from_path(file) for file in files]
+            files = self.working_directory.rglob("*")
+        files_with_keywords = self._tag_files(files)
+        return [
+            CreativeWork.from_path(file, keywords=list(keywords))
+            for file, keywords in files_with_keywords.items()
+        ]
 
-    def _find_files_directory(self, file_name_patterns: List[str]) -> List[Path]:
-        results = set()
-        for pattern in file_name_patterns:
-            paths = self.working_directory.rglob(pattern, case_sensitive=False)
-            results.update(paths)
-        return list(results)
-
-    def _find_files_git(self, file_name_patterns: List[str]) -> Optional[List[Path]]:
-        files = _git_ls_files(self.working_directory)
-        if files is None:
-            return None
-        matching_files = []
-        for file in files:
-            for pattern in file_name_patterns:
-                if file.match(pattern, case_sensitive=False):
-                    matching_files.append(file)
-        return matching_files
+    def _tag_files(self, paths: List[Path]) -> Optional[Dict[Path, Set[str]]]:
+        """Filter and tag file paths."""
+        paths_with_keywords = defaultdict(set)
+        for path in paths:
+            for pattern in self.search_pattern_list:
+                if path.match(pattern, case_sensitive=False):
+                    keywords = self.search_pattern_keywords[pattern]
+                    paths_with_keywords[path].update(keywords)
+        return paths_with_keywords
 
 
 @cache
