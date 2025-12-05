1b5656ed0b5c79102c634f7979d1ffcedaeb2963
notactuallyfinn <michael.fritzsche@dlr.de>
2025-12-05 09:14:44 +0100

    added more comments to ld_list

--- src/hermes/model/types/ld_list.py
+++ src/hermes/model/types/ld_list.py
@@ -5,6 +5,7 @@
 # SPDX-FileContributor: Michael Meinel
 # SPDX-FileContributor: Michael Fritzsche
 
+from types import NotImplementedType
 from .ld_container import (
     ld_container,
     JSON_LD_CONTEXT_DICT,
@@ -15,7 +16,7 @@ from .ld_container import (
     BASIC_TYPE,
 )
 
-from typing import Union, Self, Any
+from typing import Generator, Union, Self
 
 
 class ld_list(ld_container):
@@ -58,15 +59,15 @@ class ld_list(ld_container):
         if self.is_ld_list(data):
             if "@list" in data[0]:
                 self.container_type = "@list"
-                self.item_list = data[0]["@list"]
+                self.item_list: list = data[0]["@list"]
             elif "@graph" in data[0]:
                 self.container_type = "@graph"
-                self.item_list = data[0]["@graph"]
+                self.item_list: list = data[0]["@graph"]
             else:
                 raise ValueError("The given @set is not fully expanded.")
         else:
             self.container_type = "@set"
-            self.item_list = data
+            self.item_list: list = data
         # further validity checks
         if key == "@type":
             if any(not isinstance(item, str) for item in self.item_list) or self.container_type != "@set":
@@ -137,67 +138,192 @@ class ld_list(ld_container):
         # set the values at index to the expanded values
         self.item_list[index] = [val[0] if isinstance(val, list) else val for val in expanded_value]
 
-    def __delitem__(self, index):
+    def __delitem__(self: Self, index: Union[int, slice]) -> None:
+        """
+        Delete the item(s) at position index.
+        Note that if a deleted object is represented by an ld_container druing this process it will still exist
+        and not be modified afterwards.
+
+        :param self: The ld_list the items are deleted from.
+        :type self: Self
+        :param index: The positon(s) at which the item(s) is/ are deleted.
+        :type index: int | slice
+
+        :return:
+        :rtype: None
+        """
         del self.item_list[index]
 
-    def __len__(self):
+    def __len__(self: Self) -> int:
+        """
+        Returns the number of items in this ld_list.
+
+        :param self: The ld_list whose length is to be returned.
+        :type self: Self
+
+        :return: The length of self.
+        :rtype: int
+        """
         return len(self.item_list)
 
-    def __iter__(self):
+    def __iter__(self: Self) -> Generator[Union[BASIC_TYPE | TIME_TYPE | ld_container], None, None]:
+        """
+        Returns an iterator over the pythonized values contained in self.
+
+        :param self: The ld_list over whose items is iterated.
+        :type self: Self
+
+        :return: The Iterator over self's values.
+        :rtype: Generator[Union[BASIC_TYPE | TIME_TYPE | ld_container], None, None]
+        """
+        # return an Iterator over each value in self in its pythonized from
         for index, value in enumerate(self.item_list):
             item = self._to_python(self.key, value)
+            # add which entry an ld_container is stored at, if item is an ld_container
             if isinstance(item, ld_container):
                 item.index = index
             yield item
 
-    def __contains__(self, value):
+    def __contains__(self: Self, value: JSON_LD_VALUE) -> bool:
+        """
+        Returns whether or not value is contained in self.
+        Note that it is not directly checked if value is in self.item_list.
+        First value is expanded then it is checked if value is in self.item_list.
+        If however value is assimilated by self it is checked if all values are contained in self.item_list.
+        Also note that the checks whether the expanded value is in self.item_list is based on ld_list.__eq__.
+        That means that this value is 'contained' in self.item_list if any object in self.item_list
+        has the same @id like it or it xor the object in the item_list has an id an all other values are the same.
+
+        :param self: The ld_list that is checked if it contains value.
+        :type self: Self
+        :param value: The object being checked whether or not it is in self.
+        :type value: JSON_LD_VALUE
+
+        :return: Whether or not value is being considered to be contained in self.
+        :rtype: bool
+        """
+        # expand value
         expanded_value = self._to_expanded_json([value])
+        # empty list -> no value to check
         if len(expanded_value) == 0:
             return True
+        # call contains on all items in the expanded list if it contains more then one item
+        # and return true only if all calls return true
         if len(expanded_value) > 1:
             return all(val in self for val in expanded_value)
         self_attributes = {"parent": self.parent, "key": self.key, "index": self.index, "context": self.full_context}
+        # create a temporary list containing the expanded value
+        # check for equality with a list containg exactly one item from self.item_list for every item in self.item_list
+        # return true if for any item in self.item_list this check returns true
         if self.container_type == "@set":
             temp_list = ld_list(expanded_value, **self_attributes)
             return any(temp_list == ld_list([val], **self_attributes) for val in self.item_list)
         temp_list = ld_list([{self.container_type: expanded_value}], **self_attributes)
         return any(temp_list == ld_list([{self.container_type: [val]}], **self_attributes) for val in self.item_list)
 
-    def __eq__(self, other):
+    def __eq__(
+        self: Self,
+        other: Union[
+            "ld_list",
+            list[Union[JSON_LD_VALUE, BASIC_TYPE, TIME_TYPE, ld_container]],
+            dict[str, list[Union[JSON_LD_VALUE, BASIC_TYPE, TIME_TYPE, ld_container]]],
+        ],
+    ) -> Union[bool, NotImplementedType]:
+        """
+        Returns wheter or not self is considered to be equal to other.
+        If other is not an ld_list, it is converted first.
+        For each index it is checked if the ids of the items at index in self and other match if both have one,
+        if only one has an id all other values are compared.
+        If self or other is considered unordered the comparison is more difficult and ...
+
+        :param self: The ld_list other is compared to.
+        :type self: Self
+        :param other: The list/ container/ ld_list self is compared to.
+        :type other: ld_list | list[JSON_LD_VALUE | BASIC_TYPE | TIME_TYPE | ld_container]
+            | dict[str, list[JSON_LD_VALUE | BASIC_TYPE | TIME_TYPE | ld_container]]
+
+        :return: Whether or not self and other are considered equal.
+            If other is of the wrong type return NotImplemented instead.
+        :rtype: bool | NotImplementedType
+        """
         # TODO: ld_lists with container_type "@set" have to be considered unordered
+        # check if other has an acceptable type
         if not (isinstance(other, (list, ld_list)) or ld_list.is_container(other)):
             return NotImplemented
+
+        # convert other into an ld_list if it isn't one already
         if isinstance(other, dict):
             other = [other]
         if isinstance(other, list):
             if ld_list.is_ld_list(other):
                 other = ld_list.get_item_list_from_container(other[0])
             other = self.from_list(other, parent=self.parent, key=self.key, context=self.context)
+
+        # check if the length matches
         if len(self.item_list) != len(other.item_list):
             return False
+
+        # check for special case (= key is @type)
         if (self.key == "@type") ^ (other.key == "@type"):
             return False
         if self.key == other.key == "@type":
+            # lists will only contain string
             return self.item_list == other.item_list
+
+        # check if at each index the items are considered equal
         for index, (item, other_item) in enumerate(zip(self.item_list, other.item_list)):
+            # check if items are values
             if ((ld_container.is_typed_json_value(item) or ld_container.is_json_value(item)) and
                     (ld_container.is_typed_json_value(other_item) or ld_container.is_json_value(other_item))):
                 if not ld_container.are_values_equal(item, other_item):
                     return False
                 continue
+            # check if both contain an id and compare 
             if "@id" in item and "@id" in other_item:
-                return item["@id"] == other_item["@id"]
+                if item["@id"] != other_item["@id"]:
+                    return False
+                continue
+            # get the 'real' items (i.e. can also be ld_dicts or ld_lists)
             item = self[index]
             other_item = other[index]
+            # compare using the correct equals method
             res = item.__eq__(other_item)
             if res == NotImplemented:
+                # swap order if first try returned NotImplemented
                 res = other_item.__eq__(item)
-            if res is False or res == NotImplemented:  # res is not True
+            # return false if the second comparison also fails or one of them returned false
+            if res is False or res == NotImplemented:
                 return False
+
+        # return true because no unequal elements where found
         return True
 
-    def __ne__(self, other):
+    def __ne__(
+        self: Self,
+        other: Union[
+            "ld_list",
+            list[Union[JSON_LD_VALUE, BASIC_TYPE, TIME_TYPE, ld_container]],
+            dict[str, list[Union[JSON_LD_VALUE, BASIC_TYPE, TIME_TYPE, ld_container]]],
+        ],
+    ) -> Union[bool, NotImplementedType]:
+        """
+        Returns whether or not self and other not considered to be equal.
+        (Returns not self.__eq__(other) if the return type is bool.
+        See ld_list.__eq__ for more details on the comparison.)
+
+        :param self: The ld_list other is compared to.
+        :type self: Self
+        :param other: The list/ container/ ld_list self is compared to.
+        :type other: ld_list | list[JSON_LD_VALUE | BASIC_TYPE | TIME_TYPE | ld_container]
+            | dict[str, list[JSON_LD_VALUE | BASIC_TYPE | TIME_TYPE | ld_container]]
+
+        :return: Whether or not self and other are not considered equal.
+            If other is of the wrong type return NotImplemented instead.
+        :rtype: bool | NotImplementedType
+        """
+        # compare self and other using __eq__
         x = self.__eq__(other)
+        # return NotImplemented if __eq__ did so and else the inverted result of __eq__
         if x is NotImplemented:
             return NotImplemented
         return not x
