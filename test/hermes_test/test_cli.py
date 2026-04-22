9294d0a7c3df33cb28a5ae54c62b55adba0b398b
Kernchen, Sophie <sophie.kernchen@web.de>
2026-04-22 13:11:12 +0200

    Add good case tests for cli

--- test/hermes_test/test_cli.py
+++ test/hermes_test/test_cli.py
@@ -46,7 +46,6 @@ authors:
     assert "Plugin foo not found" in stdout_res
 
 
-@pytest.mark.dev
 def test_hermes_harvest_no_plugin(hermes_env):
     hermes_env['hermes.toml'] = "[harvest]\nsources = []\n"
     with hermes_env:
@@ -65,5 +64,60 @@ def test_hermes_process(hermes_env):
 
     with hermes_env:
         result = hermes_env.run("process")
+        test_dir = hermes_env.test_path
+        assert len(os.listdir(test_dir / ".hermes/process/result/")) == 3
+        output_file = test_dir / ".hermes/process/result/codemeta.json"
+        assert output_file.exists()
+
+    assert result.returncode == 0
+
+
+def test_hermes_curate(hermes_env, tmpdir):
+    hermes_env['hermes.toml'] = "[curate]\nplugin = \"pass_curate\""
+    hermes_env['.hermes/process/result/codemeta.json'] = "{\"@context\": [\"https://doi.org/10.5063/schema/codemeta-2.0\"], \"name\": \"hermes\", \"version\": \"0.9.0\"}"
+
+    with hermes_env:
+        result = hermes_env.run("curate")
+        test_dir = hermes_env.test_path
+        assert len(os.listdir(test_dir / ".hermes/curate/result/")) == 3
+        output_file = test_dir / ".hermes/curate/result/codemeta.json"
+
+        # One small change and it breaks, so maybe adapt it to a simpler includes test
+        content = "{\"@context\": [\"https://doi.org/10.5063/schema/codemeta-2.0\", {\"schema\": " \
+                  "\"http://schema.org/\", \"prov\": \"http://www.w3.org/ns/prov#\", \"hermes-rt\": " \
+                  "\"https://schema.software-metadata.pub/hermes-runtime/1.0/\", \"hermes\": " \
+                  "\"https://schema.software-metadata.pub/hermes-content/1.0/\"}], \"name\": \"hermes\"," \
+                  " \"version\": \"0.9.0\"}"
+        assert output_file.read_text() == content
+
+    assert result.returncode == 0
+
+
+def test_hermes_deposit(hermes_env):
+    hermes_env['hermes.toml'] = "[deposit]\ntarget = \"file\""
+    hermes_env['.hermes/curate/result/codemeta.json'] = "{}"
+
+    with hermes_env:
+        result = hermes_env.run("deposit")
+        test_dir = hermes_env.test_path
+        assert len(os.listdir(test_dir / ".hermes/deposit/file/")) == 2
+        output_file = test_dir / "hermes.json"
+        assert "@context" in output_file.read_text()
+
+    assert result.returncode == 0
+
+
+def test_hermes_postprocess(hermes_env):
+    hermes_env['hermes.toml'] = "[postprocess]\nrun = [ \"config_invenio_rdm_record_id\"] \n" \
+                                "[deposit.invenio_rdm]\ncommunities = []\n"
+    hermes_env['.hermes/deposit/invenio_rdm/result.json'] = "{\"record_id\": 1234}"
+
+    with hermes_env:
+        result = hermes_env.run("postprocess")
+        #log = hermes_env.test_path  / "hermes.log"
+        #print(log.read_text())
+        output_file = hermes_env.test_path / "hermes.toml"
+        assert output_file.read_text() == "[postprocess]\nrun = [ \"config_invenio_rdm_record_id\"] \n" \
+                                          "[deposit.invenio_rdm]\ncommunities = []\nrecord_id = 1234\n"
 
     assert result.returncode == 0
